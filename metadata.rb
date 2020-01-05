cd3f0a868cca921fc92ffff3b3452c67fcfdf258
John Losito <lositojohnj@gmail.com>
2020-01-05 15:39:43 -0500

    Support fedora in metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description 'Installs/Configures rpm-fusion'
 long_description 'Installs/Configures rpm-fusion'
 version '0.1.0'
 chef_version '>= 13.0'
+supports 'fedora'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
